4a63a5e778fd07281d357eb8be97df1d51f46246
mahdi@2626 <mahdik26@chmail.ir>
2024-02-23 12:44:32 +0330

    1

---
+++ layouts/ionicapp.vue
@@ -0,0 +1,15 @@
+<template>
+  <IonApp dir="rtl">
+    Header
+    <!-- page content will appear here -->
+    <slot />
+    Footer
+  </IonApp>
+</template>
+
+<script setup>
+import { IonApp } from "@ionic/vue"
+// useHead({
+//   viewport: 'width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover',
+// })
+</script>
